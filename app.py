c585ac997195ee0954fdee4a2956a1d3b75ceb1f
antonelo-rafa <dalavia.rafael@gmail.com>
2026-02-03 18:32:48 -0300

    criação rota cadastro

--- app.py
+++ app.py
@@ -18,7 +18,23 @@ def login():
     print(email, senha)
     return render_template('login.html')
 
+#rota do cadastro:
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    cpf = request.form.get('cpf')
+    nome = request.form.get('nome')
+    email= request.form.get('email')
+    senha= request.form.get('senha')
 
+    conexao = conexao_banco()
+    cursor = conexao.cursor()
+
+    insert_cad = 'INSERT INTO cadastro(cpf, nome, email, senha) values(%s, %s, %s, %s)'
+    valores = (cpf, nome, email, senha)
+
+    cursor.execute(insert_cad, valores)
+    conexao.commit()
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
